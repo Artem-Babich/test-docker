19dddce546cf5d08d3530912b96c0689f01b578a
Artem-Babich <51639399+Artem-Babich@users.noreply.github.com>
2023-01-31 17:51:04 +0400

    Update test.js

--- test.js
+++ test.js
@@ -1,17 +1,22 @@
 const puppeteer = require('puppeteer');
 
-console.log('Taking screenshot');
-const browser = await puppeteer.launch({
-    headless: true,
-    executablePath: '/usr/bin/google-chrome',
-    args: [
-        "--no-sandbox",
-        "--disable-gpu",
-    ]
-});
-const page = await browser.newPage();
-await page.goto('https://www.google.com');
-const imageBuffer = await page.screenshot();
-await browser.close();
+async function test () {
+    console.log('Taking screenshot');
 
-console.log('Screenshot taken');
+    const browser = await puppeteer.launch({
+        headless: true,
+        executablePath: '/usr/bin/google-chrome',
+        args: [
+            "--no-sandbox",
+            "--disable-gpu",
+        ]
+    });
+    const page = await browser.newPage();
+    await page.goto('https://www.google.com');
+    const imageBuffer = await page.screenshot();
+    await browser.close();
+
+    console.log('Screenshot taken');
+}
+
+test()
